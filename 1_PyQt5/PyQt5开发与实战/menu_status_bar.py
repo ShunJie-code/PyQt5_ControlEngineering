01c05ed2cca8adce5d7da3bff633797e82f21a5d
xsj <xiaoshunjie@qq.com>
2024-12-08 23:28:01 +0800

    在QtDesigner中为窗口添加菜单和工具栏

--- 1_PyQt5/PyQt5开发与实战/menu_status_bar.py
+++ 1_PyQt5/PyQt5开发与实战/menu_status_bar.py
@@ -1,18 +1,15 @@
 """
-22
-signal 和 slot
-可以一对多
-也可以多对一
+23 菜单栏和状态栏
 """
 import sys
-import UI_MainWinSignalSlot
+import UI_MainWinToolbar
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = QMainWindow()
-    ui = UI_MainWinSignalSlot.Ui_MainWindow()
+    ui = UI_MainWinToolbar.Ui_MainWindow()
 
     ui.setupUi(mainWindow)
     mainWindow.show()
