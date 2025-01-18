062ecb0199c2bfb3f079d87ed3446f18653c4d8f
xsj <xiaoshunjie@qq.com>
2025-01-18 22:36:04 +0800

    绘制nyquist图

--- ControlEngineering/chapter6/nyquist6_2.py
+++ ControlEngineering/chapter6/nyquist6_2.py
@@ -1,27 +1,19 @@
 """
 绘制 nyquist 轨迹
+通过开环系统幅值和相位信息来判断闭环系统是否稳定
 """
 import control as ct
 import numpy as np
 import matplotlib.pyplot as plt
-from ControlEngineering.chapter4.PredefinedFunction import *
+# from ControlEngineering.chapter4.PredefinedFunction import *
 
 
 def test():
-    fig, ax = plt.subplots(1, 2)
-    p = ct.tf([0, 1], [1, 1, 1.5, 1])
-    x, y, _ = ct.nyquist(p, np.logspace(-3, 5, 1000), plot=False)
-    ax[0].plot(x, y, color='k')
-    ax[0].plot(x, -y, ls='--', color='k')
-    ax[0].scatter(-1, 0, color='k')
-    plot_set(ax[0], 'Re', 'Im')
-
-    p = ct.tf([0, 1], [1, 2, 2, 1])
-    x, y, _ = ct.nyquist(p, np.logspace(-3, 5, 1000), Plot=False)
-    ax[1].plot(x, y, color='k')
-    ax[1].plot(x, -y, ls='--', color='k')
-    ax[1].scatter(-1, 0, color='k')
-    plot_set(ax[1], 'Re', 'Im')
+    # 定义开环传递函数
+    p1 = ct.tf([0, 1], [1, 1, 1.5, 1])  # 闭环系统不稳定，包围（-1， 0）
+    p2 = ct.tf([0, 1], [1, 2, 2, 1])    # 闭环系统稳定
+    p_list = [p1, p2]
+    ct.nyquist(p_list, np.logspace(-3, 5, 10000))
     plt.show()
 
 
--- ControlEngineering/chapter6/nyquist_data.py
+++ ControlEngineering/chapter6/nyquist_data.py
@@ -1,22 +1,24 @@
+"""
+获取 nyquist 图坐标数据
+"""
 import control as ct
-# import numpy as np
 import matplotlib.pyplot as plt
-# 定义传递函数
+
+# 1 定义传递函数
 num = [1]
 den = [1, 1]
-# sys = ct.tf(num, den)
 sys = ct.tf(num, den)
-# 绘制奈奎斯特图
+
+# 2 绘制奈奎斯特图
 sys_list = [sys]
+# ny_plt = ct.nyquist(sys_list, plot=False)  # 这一句ny_plt = 0
 ny_plt = ct.nyquist(sys_list)
-print(type(ny_plt))
-print(ny_plt)
-print(ny_plt.lines)
-# print(x.lines[0])
-plt.title('Nyquist Plot')
-plt.show()
+# print(1, type(ny_plt))
+# print(2, ny_plt)
+
+
+# 3 遍历 lines 中的每个 Line2D 对象，获取曲线坐标数据
 lines = ny_plt.lines
-# 遍历 lines 中的每个 Line2D 对象
 for i, line_list in enumerate(lines):
     for j, line in enumerate(line_list):
         if line is not None:
@@ -26,3 +28,4 @@ for i, line_list in enumerate(lines):
             print(f"子图 {i+1}, 线 {j+1} 的纵轴数据: {y_data}")
         else:
             print(f"子图 {i + 1}, 线 {j + 1} 是 None")
+plt.show()  # 阻塞函数
